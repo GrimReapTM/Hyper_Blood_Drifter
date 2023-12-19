68735e9ccfad25b045f87f28d747b13612610831
GrimReapTM <filipmatousekpost@gmail.com>
2023-12-19 18:38:14 +0100

    PATHFINDING PATHFINDING PATHFINDING PATHFINDING

--- Scenes/entity scenes/bath_messengers.gd
+++ Scenes/entity scenes/bath_messengers.gd
@@ -1,13 +1,16 @@
 extends StaticBody2D
 
-@export var ID = 0
+@export var ID = "blood"
 
+@export var player: CharacterBody2D
 
 var activated = false
 var isActivating = false
+var inTrigger = false
 
 func _on_interaction_area_entered(area):
 	if area.name == "player_hurtbox":
+		inTrigger = true
 		if not isActivating:
 			$E/Sprite2D.visible = true
 			$AnimationPlayer.play("wake_up")
@@ -16,12 +19,18 @@ func _on_interaction_area_entered(area):
 	
 func _on_interaction_area_exited(area):
 	if area.name == "player_hurtbox":
-			$E/Sprite2D.visible = false
-			$AnimationPlayer.play_backwards("wake_up")
-			$AnimationPlayer.queue("RESET")
+		inTrigger = false
+		$E/Sprite2D.visible = false
+		$AnimationPlayer.play_backwards("wake_up")
+		$AnimationPlayer.queue("RESET")
+		player.paused = false
 
 func interact():
-	pass
-			
+	if Input.is_action_just_pressed("interact") and inTrigger:
+		$AnimationPlayer.play("interact")
+		$AnimationPlayer.play("interact_idle")
+		$E/Sprite2D.visible = false
+		player.paused = true
+
 func _physics_process(_delta):
 	interact()
--- Scenes/entity scenes/enemy.gd
+++ Scenes/entity scenes/enemy.gd
@@ -9,13 +9,14 @@ const bullet = preload("res://Scenes/entity scenes/bullet.tscn")
 
 @export var defalut_pos: Marker2D
 @export var player: CharacterBody2D
-@onready var nav_agent = $navigation_agent
+@onready var nav_agent = $CollisionShape2D/navigation_agent as NavigationAgent2D
 @onready var animations = $AnimationPlayer
 
 @export var hp = 50
 @export var maxHp = hp
 signal healthChanged
 
+var queue = null
 var distance = "far"
 
 func _on_hurtbox_area_entered(area):
@@ -79,7 +80,6 @@ var saved_index = -1
 var global_index = 0
 
 func attack_calculation():
-	attacking = true
 	var player_pos = player.position
 	var base = 999999999
 	var move_pos = 0
@@ -96,6 +96,7 @@ func attack_calculation():
 
 
 func attack_melee(index):
+	attacking = true
 	if saved_index == index and combo:
 		animations.play_backwards("attack_" + attackAnimations[index])
 		combo = false
@@ -193,55 +194,76 @@ func _on_aggro_range_body_entered(body):
 # the enemy always attacks when player is in this range
 func _on_always_attack_body_entered(body):
 	if body.name == "Player":
-		attack_melee(attack_calculation())
+		distance = "attack"
 
 # exiting always attack
 func _on_always_attack_body_exited(body):
-	pass # Replace with function body.
+	if body.name == "Player":
+		distance = "close"
 
 # enemy either attacks (50%), gets even closer (25%) or circles around the player (25%)
 func _on_close_range_body_entered(body):
-	pass # Replace with function body.
+	if body.name == "Player":
+		distance = "close"
 
 # exiting close range
 func _on_close_range_body_exited(body):
-	pass # Replace with function body.
+	if body.name == "Player":
+		distance = "mid"
 
 # enemy either shoots (25%), gets even closer (25%) or circles around the player (50%)
 func _on_mid_range_body_entered(body):
-	pass # Replace with function body.
+	if body.name == "Player":
+		distance = "mid"
+
+func _on_mid_range_body_exited(body):
+	if body.name == "Player":
+		distance = "far"
 
 # enemy either shoots (25%), gets even closer (50%) or circles around the player (25%)
 func _on_long_range_body_entered(body):
-	pass # Replace with function body.
+	if body.name == "Player":
+		distance = "far"
 
 # exiting long range - always runs towards the player
 func _on_long_range_body_exited(body):
-	pass # Replace with function body.
-
+	if body.name == "Player":
+		distance = "follow"
 #---------------------------------------------------------------------------------------------
 
-func update_path():
+func _on_path_timer_timeout():
 	nav_agent.target_position = player.position
 
 func _process(_delta):
 	if raycast.enabled:
 		raycast.target_position = Vector2(player.position.x, player.position.y + 60) - position
-		if raycast.is_colliding() and str(raycast.get_collider()) == "Player:<CharacterBody2D#37094425938>":
+		if raycast.is_colliding() and "Player" in str(raycast.get_collider()):
 			aggro = true
 			raycast.enabled = false
 
 func _physics_process(delta):
+	velocity = to_local(nav_agent.get_next_path_position()).normalized() * speed * delta
 	if aggro and not attacking:
 		match distance:
+			"attack":
+				pass
 			"close":
 				pass
 			"mid":
 				pass
-			"long":
-				var dir = to_local(nav_agent.get_next_path_position()).normalized()
-				velocity = dir * speed * delta
+			"far":
+				pass
+			"follow":
+				pass
+
 	move_and_slide()
 	attack_calculation()
-
-
+	print("player position: " + str(player.position))
+	print("target position: " + str(nav_agent.target_position))
+	print("velocity: " + str(velocity))
+	print("next path position: " + str(nav_agent.get_next_path_position()))
+	print("position: " + str(position))
+	print("is reachable: " + str(nav_agent.is_target_reachable()))
+	print("is reached: " + str(nav_agent.is_target_reached()))
+	print("is finished: " + str(nav_agent.is_navigation_finished()))
+	print("-----------------------------------")
